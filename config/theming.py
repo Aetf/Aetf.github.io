05f843be82206f50b392ffbaa1238c2ab12c3085
Aetf <aetf@unlimitedcodeworks.xyz>
2015-11-16 17:04:52 -0500

    Fix link to blog page

--- config/theming.py
+++ config/theming.py
@@ -12,9 +12,14 @@
 # NOTE: THEME is relative to main config file
 THEME = '../themes/smoothie'
 
+# smoothie uses a non-standard `blog` template for the list of articles
+DIRECT_TEMPLATES = ['index', 'archives', 'blog']
+BLOG_URL = 'blog'
+BLOG_SAVE_AS = 'blog/index.html'
+
 SITESUBTITLE = 'A pessimist because of intelligence, but an optimist because of will.'
 # COVER_IMG = "images/covers/red.png"
-SITE_TITLE_LABEL = "Dheepak Krishnamurthy"
+SITE_TITLE_LABEL = "Unlimited Code Works"
 SITE_SUBTITLE_LABEL = "Engineer & Tinkerer."
 SITE_DESCRIPTION = 'My name is Aetf. This is my personal blog.'
 
