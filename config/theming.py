c1b61bd101725980ed786eae4b7d84b13949b887
Aetf <aetf@unlimitedcodeworks.xyz>
2015-12-04 15:57:41 -0500

    Almost finish about me

--- config/theming.py
+++ config/theming.py
@@ -69,24 +69,34 @@ SITE_LICENSE = """
 """
 
 # About me section in landing page
-LANDING_PAGE_ABOUT = { 'details':
-"""
+LANDING_PAGE_ABOUT = { 'details':"""
 <p>
-My name is Aetf. 
-I’ve dabbled with mobile application and web development, home automation and photography. 
-I’m currently working towards a Master’s degree in Computer Science.
+Congratuations! You have made it to the Unlimited Code Works! Welcome! 
+</p>
+<p>
+Here, I'd like to share some exciting projects I'm currently working on or have worked
+on in the past, as well as all kinds of interesting ideas I have, be it silly or not ;).
+This also serves as a way of maintaining an archive. Feel absolutely free to get in
+touch with me if you find anything interesting!
+</p>
+<p>
+Some words about myself now. My name is Peifeng Yu (Aetf is my nickname) and I'm
+currently working towards a Master's degree in Computer Science. I’ve dabbled with
+compiler and system architecture, opensource projects and japanese animes
+(Yes, animes (ﾉ>ω<)ﾉ).
 </p>
 <img src="images/coverPicture.jpg" alt="Alt text! And a picture of me!" style="width:100%">
 <p></p>
 <p>
-The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.
-The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. 
-The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. 
-The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. 
+I love reading stories. Be it animes, comics or games, it is the stories in them that
+fascinate me most. As for favorate game types? AVG, RPG, detective games and sometimes
+music games. Basically I love all games with a great story. I even tried to figure out
+the story behind
+<a href='https://en.wikipedia.org/wiki/Fate/unlimited_codes'>Fate/Unlimited Code</a>,
+a arcade fighting game :P.
 </p>
 <p>
-On this website, I intend to share interesting projects I’m currently working on
-or have worked on in the past, partly also as a way of establishing an archive.
-If you find anything interesting, feel absolutely free to get in touch with me.
+Anyway, hope my articles could help you or at least bring you some fun. Live long and
+prosper...and el psy congroo!
 </p>
 """}
