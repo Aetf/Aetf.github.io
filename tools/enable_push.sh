9c7b59f1f1477ce1e40fc22fdcc40ba1136a102b
Aetf <aetf@unlimitedcodeworks.xyz>
2017-11-06 21:54:25 -0500

    Fix: git submodule should use https url for fetch

---
+++ tools/enable_push.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+git submodule update --init
+
+add_push() {
+    local repo=$1
+    local push_url=$2
+    pushd $repo >/dev/null
+    echo "Entering $repo"
+    origin=$(git remote get-url origin)
+    origin="git@github.com:${origin##'https://github.com/'}"
+    git remote set-url --push origin $origin
+    git checkout master
+    popd >/dev/null
+}
+
+add_push themes/smoothie
