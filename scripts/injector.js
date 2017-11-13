e1e4fcce95de9e1e50bdcabc87b8d8208bb0932d
Aetf <aetf@unlimitedcodeworks.xyz>
2017-11-12 20:52:41 -0500

    Change: inject custom js and css without modifying theme

--- scripts/injector.js
+++ scripts/injector.js
@@ -1,21 +1,38 @@
 'use strict';
 
+const fs = require('hexo-fs');
+const pathFn = require('path');
+const _ = require('lodash');
+
 const KATEX_INLINE_MARKER = "<span class=\"katex\">";
 const KATEX_BLOCK_MARKER = "<span class=\"katex-display\">";
+
+function shouldInjectKaTex (src) {
+    var should = src.indexOf(KATEX_INLINE_MARKER) >= 0 || src.indexOf(KATEX_BLOCK_MARKER) >= 0;
+    return should;
+}
 
 function Injector(hexo) {
     this.hexo = hexo;
 }
 
-Injector.prototype._shouldInject = function (src) {
-    var should = src.indexOf(KATEX_INLINE_MARKER) >= 0 || src.indexOf(KATEX_BLOCK_MARKER) >= 0;
-    return should;
-}
-
 Injector.prototype._inject = function (inject) {
     const css = 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.6.0/katex.min.css';
-    inject.headEnd.link({ rel: 'stylesheet', href: css },
-                        { shouldInject: this._shouldInject.bind(this) });
+    inject.headEnd.link({ rel: 'stylesheet', href: css }, { shouldInject: shouldInjectKaTex });
+
+    const siteRoot = pathFn.join(__dirname, '..', 'source');
+    const injectedAssets = pathFn.join(siteRoot, 'assets', 'theme', 'inject');
+    const files = fs.listDirSync(injectedAssets);
+    files.map(el => pathFn.join(injectedAssets, el))
+        .map(el => pathFn.relative(siteRoot, el))
+        .map(el => hexo.config.root + el)
+        .forEach(el => {
+        if (_.endsWith(el, '.js')) {
+            inject.bodyEnd.script({ type: 'text/javascript', src: el });
+        } else if (_.endsWith(el, '.css')) {
+            inject.headEnd.link({ rel: 'stylesheet', href: el });
+        }
+    });
 };
 
 Injector.prototype.register = function () {
