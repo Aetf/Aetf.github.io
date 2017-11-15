6b65d1258f29db7c60db0006a4ece4f8fc132587
Aetf <aetf@unlimitedcodeworks.xyz>
2017-11-14 19:56:29 -0500

    Change: style tweak for site title and back-to-top button

--- scripts/injector.js
+++ scripts/injector.js
@@ -12,27 +12,51 @@ function shouldInjectKaTex (src) {
     return should;
 }
 
+const INJECTED_FILES = [
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    {
+        path: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.6.0/katex.min.css',
+        shouldInject: shouldInjectKaTex
+    }
+];
+
+/**
+ * Fallback as css
+ * @param {*} inject 
+ * @param {*} path 
+ * @param {*} opts 
+ */
+function injectBasedOnExt(inject, path, opts) {
+    if (_.endsWith(path, '.js')) {
+        inject.bodyEnd.script({ type: 'text/javascript', src: path }, opts);
+    } else {
+        // treat anything not ending in js as css
+        inject.headEnd.link({ rel: 'stylesheet', href: path }, opts);
+    }
+}
+
 function Injector(hexo) {
     this.hexo = hexo;
 }
 
 Injector.prototype._inject = function (inject) {
-    const css = 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.6.0/katex.min.css';
-    inject.headEnd.link({ rel: 'stylesheet', href: css }, { shouldInject: shouldInjectKaTex });
+    // Inject all third party files
+    INJECTED_FILES.forEach(el => {
+        if (_.isString(el)) {
+            injectBasedOnExt(inject, el);
+        } else {
+            injectBasedOnExt(inject, el.path, { shouldInject: el.shouldInject });
+        }
+    }, this);
 
+    // Inject all local files found in /assets/theme/inject
     const siteRoot = pathFn.join(__dirname, '..', 'source');
     const injectedAssets = pathFn.join(siteRoot, 'assets', 'theme', 'inject');
     const files = fs.listDirSync(injectedAssets);
     files.map(el => pathFn.join(injectedAssets, el))
         .map(el => pathFn.relative(siteRoot, el))
         .map(el => hexo.config.root + el)
-        .forEach(el => {
-        if (_.endsWith(el, '.js')) {
-            inject.bodyEnd.script({ type: 'text/javascript', src: el });
-        } else if (_.endsWith(el, '.css')) {
-            inject.headEnd.link({ rel: 'stylesheet', href: el });
-        }
-    });
+        .forEach(el => injectBasedOnExt(inject, el));
 };
 
 Injector.prototype.register = function () {
