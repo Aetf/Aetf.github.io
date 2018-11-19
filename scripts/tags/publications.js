1c186a9102e1920d6773bb273a690f029b01ad5a
Aetf <aetf@unlimitedcodeworks.xyz>
2018-11-19 13:16:39 -0500

    upd

--- scripts/tags/publications.js
+++ scripts/tags/publications.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const pathFn = require('path');
+const fs = require('hexo-fs');
 
 function bindHelpers(locals) {
     const helpers = hexo.extend.helper.list();
@@ -16,12 +17,25 @@ function bindHelpers(locals) {
 }
 
 /**
+ * Async, returns Promise
+ * @param {*} publications 
+ */
+function normalizePubItems(publications) {
+    const pubDir = publications.publication_dir;
+    return fs.stat(pubDir).then(stats => {
+        if (!stats.isDirectory()) return [];
+    }).then(() => {
+        // TODO: FIXME
+    });
+}
+
+/**
  *
  * @param {*} args Arguments to the tag. An array of string, they are whitespace splited.
  */
 function publicationsTag(args) {
     const widgets_dir = pathFn.join(hexo.base_dir, 'widgets');
-    const locals = {
+    const locals = bindHelpers({
         args: args,
         // emulate hexo's own local environment in the rendering
         config: hexo.config,
@@ -31,11 +45,14 @@ function publicationsTag(args) {
         env: hexo.env,
         page: this,
         view_dir: widgets_dir
-    };
+    });
 
-    return hexo.render.render({
-        path: pathFn.join(widgets_dir, 'pub_item.ejs')
-    }, bindHelpers(locals));
+    return hexo.execFilter('template_locals', locals, { context: hexo })
+        .then(locals =>
+            hexo.render.render({
+                path: pathFn.join(widgets_dir, 'pub_item.ejs')
+            }, locals)
+        );
 }
 
 hexo.extend.tag.register('publications', publicationsTag, { ends: false, async: true });
