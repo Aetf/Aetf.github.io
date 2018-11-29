6fab9d1c555710bc28cbba55349f63ee5b3dc953
Aetf <aetf@unlimitedcodeworks.xyz>
2018-11-28 21:53:05 -0500

    Fix: post links generate wrong href when fragment is empty

--- scripts/tags/post_link.js
+++ scripts/tags/post_link.js
@@ -1,31 +1,38 @@
 'use strict';
 
+const { htmlTag } = require('hexo-util');
+const Post = hexo.model('Post');
+
 /**
  * Post link tag
  *
  * Syntax:
  *   {% post_link slug[#fragment] [title] %}
  */
-const Post = hexo.model('Post');
-
 function postLinkTag(args) {
-    var slug = args.shift();
-    var frag = '';
+    let slug = args.shift();
 
     if (!slug) return;
 
+    let frag = '';
     [slug, frag] = slug.split('#');
+    if (!frag) {
+        frag = '';
+    }
 
-    if (frag && frag.length > 0) {
+    if (frag.length > 0) {
         frag = '#' + frag;
     }
 
-    var post = Post.findOne({ slug: slug });
+    let post = Post.findOne({ slug });
     if (!post) return;
 
-    var title = args.length ? args.join(' ') : post.title;
+    let title = args.length ? args.join(' ') : post.title;
 
-    return '<a href="' + hexo.config.root + post.path + frag + '" title="' + title + '">' + title + '</a>';
+    return htmlTag('a', {
+        href: hexo.config.root + post.path + frag,
+        title: title
+    }, title);
 };
 
 hexo.extend.tag.register('post_link', postLinkTag, { ends: false });
