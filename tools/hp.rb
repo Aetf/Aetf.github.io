d4c41db6792142f551accddcacc1fad4766b8595
Aetf <aetf@unlimitedcodeworks.xyz>
2020-03-04 00:03:48 -0600

    Fix: CI

--- tools/hp.rb
+++ tools/hp.rb
@@ -1,12 +1,14 @@
-#! /usr/bin/ruby
+#! /usr/bin/env ruby
 require 'html-proofer'
 
-options = { 
+options = {
   allow_hash_href: true,
   check_favicon: true,
   check_opengraph: true,
   check_html: true,
   internal_domains: ['unlimitedcodeworks.xyz', 'unlimited-code.works'],
+  # cache timeframe
+  timeframe: '6w',
   # LinkedIn denies requests
   http_status_ignore: [999],
   # XXX: remove this after hexo-excerpt implements link rewrite in excerpt
