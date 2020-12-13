0ab75e1ec1a9ca22ed78f4e73dd7ac8ba8065b8b
Aetf <aetf@unlimited-code.works>
2020-12-13 17:53:08 -0500

    Change: email tooltip

--- source/assets/theme/inject/main.js
+++ source/assets/theme/inject/main.js
@@ -40,13 +40,11 @@ $(document).on('bootstrap:before', function () {
     // a popup explains email
     var $emailLink = $('#sidebar .links-of-author-item a[title="E-Mail"]');
     if ($emailLink.length > 0) {
-        const contentText = ['My email address is my <a href="/about" >nickname</a> at this website.',
-                             'Another address for academic use is included in my',
-                             '<a href="/assets/dl/cv.pdf" target="_blank">CV</a>.',
-                             '<br />',
-                             '<span class="small">If you can\'t figure it out from the hints, well,',
-                             'you might find emailing more of a challenge than',
-                             'figuring out my address. ;)</span>'].join(' ');
+        const contentText = [
+            'My email address is my <a href="/about" >nickname</a> at this website.',
+            'Another address for academic use is included in my',
+            '<a href="/assets/dl/cv.pdf" target="_blank">CV</a>.',
+        ].join(' ');
         // Initialize jBox
         $emailLink.addClass('email_tooltip_open');
         new jBox('Tooltip', {
